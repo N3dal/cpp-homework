384b325dac23ea681d79b3af68bcb7e9b5a37723
N3dal <nedalxzo@gmail.com>
2022-07-26 22:12:36 +0200

    create the 01 homework

---
+++ q01.cpp
@@ -0,0 +1,25 @@
+/* Question 01:
+Write a program in C++ to print a welcome text in a separate line?
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    cout << "W";
+    cout << "e";
+    cout << "l";
+    cout << "c";
+    cout << "o";
+    cout << "m";
+    cout << "e";
+    cout << endl;
+
+
+
+    return 0;
+}
+
