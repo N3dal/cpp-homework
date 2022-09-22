e3760eb5cc14b7c27d5aed2121b3e4e506675539
N3dal <nedalxzo@gmail.com>
2022-09-22 21:34:23 +0200

    move all cpp files to spicefic dir

---
+++ cpp_files/q02.cpp
@@ -0,0 +1,21 @@
+/* Question 02 
+Write a program in C++ to print the sum of two float numbers?
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    // create two float numbers and initialize them with random numbers.
+    float first_number=3.14, second_number=2.7;
+
+    cout << "The sum of Two floats number = " << first_number + second_number << endl;
+
+
+
+    return 0;
+}
+
